ff5bf0fd8e05634c0c14eb7ca18bbaaa35ccf373
Derek Harter <derek@harter.pro>
2026-05-29 12:05:05 -0500

    Update python versions in github actions
    Update the python versions output to show available devices
    in the github actions output.

--- src/python_versions.py
+++ src/python_versions.py
@@ -1,9 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 import sys
 import numpy as np
 import sklearn as sk
 import matplotlib as mp
 import scipy as sp
 import pandas as pd
+import tensorflow as tf
 
 print('Python version    : ', sys.version)
 print('numpy version     : ', np.__version__)
@@ -11,3 +15,9 @@ print('scikit-learn  vers: ', sk.__version__)
 print('matplotlib version: ', mp.__version__)
 print('scipy version     : ', sp.__version__)
 print('pandas version    : ', pd.__version__)
+print('tensorflow version: ', tf.__version__)
+
+dev = tf.config.list_physical_devices()
+print('Physical Devices  : ', dev)
+dev = tf.config.list_logical_devices()
+print('Available Devices : ', dev)
